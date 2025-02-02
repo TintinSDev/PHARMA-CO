71d9667cc71c6ddbb8fb4608a1285f65950b22a0
Martin <mmaina290@gmail.com>
2025-02-02 11:21:58 +0300

    Yarn build for new assets installed

--- src/components/PatientList.jsx
+++ src/components/PatientList.jsx
@@ -1,44 +1,3 @@
-// import { useState, useEffect } from "react";
-// import axios from "axios";
-
-// function PatientList() {
-//   const [patients, setPatients] = useState([]);
-//   const [loading, setLoading] = useState(true);
-//   const [error, setError] = useState(null);
-
-//   useEffect(() => {
-//     fetchPatients();
-//   }, []);
-
-//   const fetchPatients = async () => {
-//     try {
-//       const response = await axios.get("http://localhost:5174/patients");
-//       setPatients(response.data);
-//     } catch (error) {
-//       setError("Failed to fetch patients. Please try again later.");
-//     } finally {
-//       setLoading(false); // Ensure loading stops regardless of success or error
-//     }
-//   };
-
-//   if (loading) return <p>Loading patients...</p>;
-//   if (error) return <p>{error}</p>;
-
-//   return (
-//     <div className="patient-list">
-//       <h2>Patients</h2>
-//       <ul>
-//         {patients.map((patient) => (
-//           <li key={patient.id}>
-//             {patient.first_name} {patient.middle_name} {patient.last_name}
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// }
-
-// export default PatientList;
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
@@ -94,7 +53,7 @@ function PatientList() {
             {/* Trash bin animation when patient is deleted */}
             {deletedPatients.includes(patient.id) && (
               <>
-                {Array.from({ length: 7 }).map((_, index) => (
+                {Array.from({ length: 10 }).map((_, index) => (
                   <motion.img
                     key={index}
                     src={trashBin}
